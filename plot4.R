cbc750ae3a31ab5be8b5fda2ad03dc76253eda1c
Alexandra Silva <alexandranobresilva@gmail.com>
2017-04-24 18:19:09 +0100

    Assigment documentation

---
+++ plot4.R
@@ -0,0 +1,29 @@
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+library(ggplot2)
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
+# which of these four sources have seen decreases in emissions from 1999 2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999 2008? 
+# Use the ggplot2 plotting system to make a plot answer this question.
+
+# Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+
+# fetch all NEIxSCC records with Short.Name (SCC) Coal
+coalMatches  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
+subsetNEISCC <- NEISCC[coalMatches, ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEISCC, sum)
+
+
+
+png("plot4.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from coal sources from 1999 to 2008')
+print(g)
+dev.off()
